744630b10a8ca5fbcc693825fbe972e18cd4eecb
Dree0313 <alexandria.marbury@snhu.edu>
2025-12-01 17:48:46 -0800

    Second Commit

---
+++ overlay.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+    const overlay = document.getElementById('welcome-overlay');
+
+    //Slide down after 0.5s
+    setTimeout(() => {
+        overlay.classList.add('show');
+    }, 500);
+
+    //Slide back up after 3.0s
+    setTimeout(() => {
+        overlay.classList.remove('show');
+    }, 3500);
+})

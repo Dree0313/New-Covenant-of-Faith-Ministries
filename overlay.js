5064b198f5c441fe96f2a71d756a5cceabc35b64
Dree0313 <alexandria.marbury@snhu.edu>
2025-12-03 11:29:11 -0800

    logo update

--- overlay.js
+++ overlay.js
@@ -1,16 +1,27 @@
 window.addEventListener('load', () => {
     const overlay = document.getElementById('welcome-overlay');
+    const logoOverlay = document.getElementById('logo-overlay')
     const menuButton = document.getElementById('menu-button');
+
 
     //Slide down after 0.5s
     setTimeout(() => {
         overlay.classList.add('show');
     }, 500);
 
+    setTimeout(() => {
+        logoOverlay.classList.add('visible');
+    }, 1000);
+
+    setTimeout(() => {
+        overlay.querySelector('.overlay-content').classList.add('hidden-content');
+    }, 4000);
+
     //Slide back up after 4.0s
     setTimeout(() => {
         overlay.classList.remove('show');
-    }, 4500);
+        logoOverlay.classList.add('shrink');
+    }, 5000);
 
     setTimeout(() => {
         menuButton.classList.remove('menu-hidden');
